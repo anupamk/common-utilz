9e4db581f625137e6ead25c4846765e0d267963f
Anupam Kapoor <anupam.kapoor@gmail.com>
2014-01-11 13:01:00 +0530

    run tests and benchmarks

---
+++ run-tests-and-benchmark.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#
+# this function runs unit tests on all the packages that are defined
+# here.
+# 
+function run_unit_tests() {
+    echo "*** Package Unit Tests ***"
+    go test ./...
+}
+
+#
+# this function runs benchmark tests on all the packages that are
+# defined here.
+# 
+function run_benchmarks() {
+    echo "*** Package Benchmarks ***"
+    pkg_list=`find . -maxdepth 1 -type d | egrep -v -e '(git|\.$)'`
+    for pkg in $pkg_list
+    do
+	echo "- `basename $pkg`"
+	go test -run=NONE -bench=. $pkg | grep Benchmark | awk '{print "    -", $1, "\t", $3, "\t", $4}'
+    done
+}
+
+function main() {
+    run_unit_tests
+    run_benchmarks
+}
+
+# start it all
+main
