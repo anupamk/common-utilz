02fb39ee946b89642a4929fcd29edf0cdf9fb2d1
Anupam Kapoor <anupam.kapoor@gmail.com>
2014-01-15 10:24:54 +0530

    a few commonly used slice operations

---
+++ slice_utils/slice_cmp_utils.go
@@ -0,0 +1,68 @@
+//
+// Copyright (c) 2014, Anupam Kapoor. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Author: anupam.kapoor@gmail.com (Anupam Kapoor)
+//
+//
+// this package implements a bunch of convenience routines for working
+// with slices.
+//
+// this file implements slice-comparison routines.
+//
+package slice_utils
+
+//
+// this function returns true if two int slices are equal i.e. for
+// all i, x[i] == y[i] and 0 <= i < len(x)
+//
+func CmpIntSlice(x, y *[]int) bool {
+	if len(*x) != len(*y) {
+		return false
+	}
+
+	for i, xv := range *x {
+		if xv != (*y)[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+//
+// this function returns true if two int32 slices are equal i.e. for
+// all i, x[i] == y[i] and 0 <= i < len(x)
+//
+func CmpInt32Slice(x, y *[]int32) bool {
+	if len(*x) != len(*y) {
+		return false
+	}
+
+	for i, xv := range *x {
+		if xv != (*y)[i] {
+			return false
+		}
+	}
+
+	return true
+}
