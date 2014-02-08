5e013bbfb490f0df8e43e5b7423e681d1175b39b
anupam kapoor <anupam.kapoor@gmail.com>
2014-02-08 23:29:55 +0530

    topological sort implementation

---
+++ graph/algorithms/topological_sort.go
@@ -0,0 +1,41 @@
+package algorithms
+
+import (
+	"error"
+	"fmt"
+	"github.com/anupamk/common-utilz/graph"
+	"github.com/anupamk/common-utilz/traversal"
+)
+
+type Topological struct {
+	order []int32
+}
+
+//
+// this function computes the topological order for a given
+// digraph. returns an error if no such ordering is possible
+//
+func ComputeTopologicalOrder(G graph.GraphOps) (TO *Topological, err error) {
+	if dg := DigraphCyle(G); !dg.IsAcyclic() {
+		err = fmt.Errorf("error: no ordering possible. digraph has cycle : %s\n", dg.Cycle())
+		return
+	}
+
+	//
+	// reverse-post-order traversal is the topological sort
+	// order.
+	//
+	dg_to := traversal.DoDFSTraversals(G)
+	topo_order := dg_to.ReversePost()
+
+	TO = &Topological{
+		order: make([]int32, len(topo_order)),
+	}
+
+	copy(TO.order, topo_order)
+
+	return
+}
+
+func (TO *Topological) Order() []int32 { return TO.order }
+func (TO *Topological) IsDAG() bool    { return len(TO.order) != 0 }
